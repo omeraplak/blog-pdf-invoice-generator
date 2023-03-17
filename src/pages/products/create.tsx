aa660f6766505dd0483cac385753edb4b99e91b0
Abdullah Numan <anewman15@hotmail.com>
2023-03-18 03:39:57 +0600

    feat(blog-pdf-invoice-generator): remove example pages

--- src/pages/products/create.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { AntdCreateInferencer } from "@refinedev/inferencer/antd";
-
-export const ProductCreate: React.FC<IResourceComponentsProps> = () => {
-  return <AntdCreateInferencer />;
-};
--- src/pages/products/list.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { AntdListInferencer } from "@refinedev/inferencer/antd";
-
-export const ProductList: React.FC<IResourceComponentsProps> = () => {
-  return <AntdListInferencer />;
-};
