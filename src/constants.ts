f5a9214608e75b31b3c5597ed752b56a2e54e301
Abdullah Numan <anewman15@hotmail.com>
2023-03-18 03:38:43 +0600

    feat(blog-pdf-invoice-generator): update strapi access details

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
-export const TOKEN_KEY = "strapi-jwt-token";
+export const API_URL = "http://localhost:1337";
+export const TOKEN_KEY = "625b118353b2924b459527cd39f7ca792a870cc13619562a3e3f8ee6908519c581bcabb8152cbd10913e72d9adf725e6bd99b8793632b34d1dd952544e3bd883eaba7c3ab169308cd29730267247147d20af102d70a311d515d9b5ab06384e0a2418fe47ecda895d74d87bbcf6bbc74d9b318d5795fcf7be1691ed4524d73621";
