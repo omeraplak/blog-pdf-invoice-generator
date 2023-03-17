2dfaa4a55eeffe8c7676c5dd03b5754674357edc
Abdullah Numan <anewman15@hotmail.com>
2023-03-18 03:46:44 +0600

    feat(blog-pdf-invoice-generator): fix CORS settings for strapi app

--- strapi-pdf-invoice-generator/config/middlewares.js
+++ strapi-pdf-invoice-generator/config/middlewares.js
@@ -1,8 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://localhost:3000']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
